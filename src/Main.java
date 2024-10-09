5db379d63342adf3b373bfc7f86831095a3be31c
Karen <karapetyankaren33933@gmail.com>
2024-10-09 22:58:00 +0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -150,5 +150,6 @@ public class Main {
         System.out.println(" Маша теперь получает " + mashaNewSalary + " рублей. Годовой доход вырос " + mashaDifferenceBetweenAnnualIncome + " рублей ");
         System.out.println(" Денис теперь получает " + denisNewSalary + " рублей. Годовой доход вырос " + denisDifferenceBetweenAnnualIncome + " рублей ");
         System.out.println(" Кристина теперь получает " + kristinaNewSalary + " рублей. Годовой доход вырос " + kristinaDifferenceBetweenAnnualIncome + " рублей ");
+
     }
     }
